b2c17f51a502302fc26e1c245048f2f0d6829fae
Edito Tchokoso <editotchocosso13@gmail.com>
2025-05-12 13:38:55 +0100

    Create a register schema

--- src/types/schemas.d.ts
+++ src/types/schemas.d.ts
@@ -1,3 +1,5 @@
 import loginSchema from "@/schemas/loginSchema";
+import registerSchema from "@/schemas/registerSchema";
 
 export type LoginFormData = z.infer<typeof loginSchema>;
+export type RegisterFormData = z.infer<typeof registerSchema>;
