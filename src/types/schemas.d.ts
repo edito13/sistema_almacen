47bce5b4e351ba183394a4f261ead6209b998371
Edito Tchokoso <editotchocosso13@gmail.com>
2025-05-20 14:11:21 +0100

    Export ExitFormData

--- src/types/schemas.d.ts
+++ src/types/schemas.d.ts
@@ -1,7 +1,9 @@
+import exitSchema from "@/schemas/exitSchema";
 import loginSchema from "@/schemas/loginSchema";
 import entrySchema from "@/schemas/entrySchema";
 import registerSchema from "@/schemas/registerSchema";
 
+export type ExitFormData = z.infer<typeof exitSchema>;
 export type EntryFormData = z.infer<typeof entrySchema>;
 export type LoginFormData = z.infer<typeof loginSchema>;
 export type RegisterFormData = z.infer<typeof registerSchema>;
