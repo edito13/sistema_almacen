bc0c19cdb66a952499df91d3d2d3e97709d1387d
Edito Tchokoso <editotchocosso13@gmail.com>
2025-05-21 11:13:49 +0100

    New change with new modal

--- src/types/schemas.d.ts
+++ src/types/schemas.d.ts
@@ -1,9 +1,13 @@
 import exitSchema from "@/schemas/exitSchema";
 import loginSchema from "@/schemas/loginSchema";
 import entrySchema from "@/schemas/entrySchema";
+import categorySchema from "@/schemas/categorySchema";
 import registerSchema from "@/schemas/registerSchema";
+import equipmentSchema from "@/schemas/equipmentSchema";
 
 export type ExitFormData = z.infer<typeof exitSchema>;
 export type EntryFormData = z.infer<typeof entrySchema>;
 export type LoginFormData = z.infer<typeof loginSchema>;
+export type CategoryFormData = z.infer<typeof categorySchema>;
 export type RegisterFormData = z.infer<typeof registerSchema>;
+export type EquipmentFormData = z.infer<typeof equipmentSchema>;
